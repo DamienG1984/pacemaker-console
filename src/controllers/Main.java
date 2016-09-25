44815aa0e92d17b8bcd56baca4e00a9cc4f09e7f
DamienG1984 <Damo05@gmail.com>
2016-09-25 19:44:56 +0100

    Add project from Lab download as i was unable to find one off the jar files

---
+++ src/controllers/Main.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import com.google.common.base.Optional;
+
+import models.User;
+import asg.cliche.Command;
+import asg.cliche.Param;
+import asg.cliche.Shell;
+import asg.cliche.ShellFactory;
+
+public class Main
+{
+  PacemakerAPI paceApi = new PacemakerAPI();
+  
+  @Command(description="Create a new User")
+  public void createUser (@Param(name="first name") String firstName, @Param(name="last name") String lastName, 
+                          @Param(name="email")      String email,     @Param(name="password")  String password)
+  {
+    paceApi.createUser(firstName, lastName, email, password);
+  }
+  
+  @Command(description="Create a new User")
+  public void getUser (@Param(name="email") String email)
+  {
+    User user = paceApi.getUserByEmail(email);
+    System.out.println(user);
+  }
+  
+  @Command(description="Create a new User")
+  public void getUsers ()
+  {
+    Collection<User> users = paceApi.getUsers();
+    System.out.println(users);
+  }
+
+  @Command(description="Delete a User")
+  public void deleteUser (@Param(name="email") String email)
+  {
+    Optional<User> user = Optional.fromNullable(paceApi.getUserByEmail(email));
+    if (user.isPresent())
+    {
+      paceApi.deleteUser(user.get().id);
+    }
+  }
+
+  public static void main(String[] args) throws IOException
+  {
+    Shell shell = ShellFactory.createConsoleShell("pc", "Welcome to pcemaker-console - ?help for instructions", new Main());
+    shell.commandLoop(); 
+  }
+}
---
+++ src/models/User.java
@@ -0,0 +1,45 @@
+package models;
+
+import static com.google.common.base.Objects.toStringHelper;
+import com.google.common.base.Objects;
+
+public class User 
+{
+  public String firstName;
+  public String lastName;
+  public String email;
+  public String password;
+  
+  static Long   counter = 0l;
+
+  public Long   id;
+  
+  public User()
+  {
+  }
+  
+  public User(String firstName, String lastName, String email, String password)
+  {
+    this.id        = counter++;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+  }
+  
+  @Override
+  public String toString()
+  {
+    return toStringHelper(this).addValue(firstName)
+                               .addValue(lastName)
+                               .addValue(password)
+                               .addValue(email)                               
+                               .toString();
+  }
+  
+  @Override  
+  public int hashCode()  
+  {  
+     return Objects.hashCode(this.lastName, this.firstName, this.email, this.password);  
+  }  
+}
